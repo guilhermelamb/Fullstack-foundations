fb4f10bf6d188f4eb7485b805ce7439f0da074c6
Guilherme <guilhermelamb@gmail.com>
2021-01-24 14:19:16 -0300

    Updated database configurations

--- database_setup.py
+++ database_setup.py
@@ -12,6 +12,12 @@ class Restaurant(Base):
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
 
+    @property
+    def serialize(self):
+        return {
+            'name':self.name
+        }
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
     name = Column(String(80), nullable=False)
